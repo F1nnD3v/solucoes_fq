e8749d905d5d57c6eab3cd42aedec889ba3b7815
uauauTiagO <tiagoclaudinoduarte2005@gmail.com>
2022-05-02 11:21:17 +0100

    .

--- Solucoes_fq/lab.Designer.cs
+++ Solucoes_fq/lab.Designer.cs
@@ -29,45 +29,111 @@ namespace Solucoes_fq
         /// </summary>
         private void InitializeComponent()
         {
-            this.barAddSoluto = new System.Windows.Forms.VScrollBar();
-            this.btnSolucao = new System.Windows.Forms.Button();
+            this.btnAddSolucao = new System.Windows.Forms.Button();
+            this.btnRemoveSolucao = new System.Windows.Forms.Button();
+            this.lblSolucao = new System.Windows.Forms.Label();
+            this.cboxSolucoes = new System.Windows.Forms.ComboBox();
+            this.lblSoluto = new System.Windows.Forms.Label();
+            this.btnRemoveSoluto = new System.Windows.Forms.Button();
+            this.btnAddSoluto = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // barAddSoluto
+            // btnAddSolucao
             // 
-            this.barAddSoluto.Location = new System.Drawing.Point(303, 177);
-            this.barAddSoluto.Name = "barAddSoluto";
-            this.barAddSoluto.Size = new System.Drawing.Size(17, 80);
-            this.barAddSoluto.TabIndex = 0;
-            this.barAddSoluto.Scroll += new System.Windows.Forms.ScrollEventHandler(this.barAddSoluto_Scroll);
+            this.btnAddSolucao.Location = new System.Drawing.Point(188, 256);
+            this.btnAddSolucao.Name = "btnAddSolucao";
+            this.btnAddSolucao.Size = new System.Drawing.Size(75, 23);
+            this.btnAddSolucao.TabIndex = 4;
+            this.btnAddSolucao.Text = "+";
+            this.btnAddSolucao.UseVisualStyleBackColor = true;
+            this.btnAddSolucao.Click += new System.EventHandler(this.btnAdd_Click);
             // 
-            // btnSolucao
+            // btnRemoveSolucao
             // 
-            this.btnSolucao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.btnSolucao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSolucao.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnSolucao.Location = new System.Drawing.Point(468, 247);
-            this.btnSolucao.Name = "btnSolucao";
-            this.btnSolucao.Size = new System.Drawing.Size(82, 10);
-            this.btnSolucao.TabIndex = 1;
-            this.btnSolucao.UseVisualStyleBackColor = false;
+            this.btnRemoveSolucao.Location = new System.Drawing.Point(188, 286);
+            this.btnRemoveSolucao.Name = "btnRemoveSolucao";
+            this.btnRemoveSolucao.Size = new System.Drawing.Size(75, 23);
+            this.btnRemoveSolucao.TabIndex = 5;
+            this.btnRemoveSolucao.Text = "-";
+            this.btnRemoveSolucao.UseVisualStyleBackColor = true;
+            this.btnRemoveSolucao.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // lblSolucao
+            // 
+            this.lblSolucao.AutoSize = true;
+            this.lblSolucao.Location = new System.Drawing.Point(290, 262);
+            this.lblSolucao.Name = "lblSolucao";
+            this.lblSolucao.Size = new System.Drawing.Size(188, 16);
+            this.lblSolucao.TabIndex = 6;
+            this.lblSolucao.Text = "Quantidade de solução: 200ml";
+            // 
+            // cboxSolucoes
+            // 
+            this.cboxSolucoes.FormattingEnabled = true;
+            this.cboxSolucoes.Location = new System.Drawing.Point(489, 256);
+            this.cboxSolucoes.Name = "cboxSolucoes";
+            this.cboxSolucoes.Size = new System.Drawing.Size(121, 24);
+            this.cboxSolucoes.TabIndex = 7;
+            this.cboxSolucoes.SelectedIndexChanged += new System.EventHandler(this.cboxSolucoes_SelectedIndexChanged);
+            // 
+            // lblSoluto
+            // 
+            this.lblSoluto.AutoSize = true;
+            this.lblSoluto.Location = new System.Drawing.Point(629, 262);
+            this.lblSoluto.Name = "lblSoluto";
+            this.lblSoluto.Size = new System.Drawing.Size(176, 16);
+            this.lblSoluto.TabIndex = 8;
+            this.lblSoluto.Text = "Quantidade de soluto: 200ml";
+            // 
+            // btnRemoveSoluto
+            // 
+            this.btnRemoveSoluto.Location = new System.Drawing.Point(811, 285);
+            this.btnRemoveSoluto.Name = "btnRemoveSoluto";
+            this.btnRemoveSoluto.Size = new System.Drawing.Size(75, 23);
+            this.btnRemoveSoluto.TabIndex = 10;
+            this.btnRemoveSoluto.Text = "-";
+            this.btnRemoveSoluto.UseVisualStyleBackColor = true;
+            this.btnRemoveSoluto.Click += new System.EventHandler(this.btnRemoveSoluto_Click);
+            // 
+            // btnAddSoluto
+            // 
+            this.btnAddSoluto.Location = new System.Drawing.Point(811, 255);
+            this.btnAddSoluto.Name = "btnAddSoluto";
+            this.btnAddSoluto.Size = new System.Drawing.Size(75, 23);
+            this.btnAddSoluto.TabIndex = 9;
+            this.btnAddSoluto.Text = "+";
+            this.btnAddSoluto.UseVisualStyleBackColor = true;
+            this.btnAddSoluto.Click += new System.EventHandler(this.btnAddSoluto_Click);
             // 
             // lab
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnSolucao);
-            this.Controls.Add(this.barAddSoluto);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnRemoveSoluto);
+            this.Controls.Add(this.btnAddSoluto);
+            this.Controls.Add(this.lblSoluto);
+            this.Controls.Add(this.cboxSolucoes);
+            this.Controls.Add(this.lblSolucao);
+            this.Controls.Add(this.btnRemoveSolucao);
+            this.Controls.Add(this.btnAddSolucao);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "lab";
             this.Text = "lab";
+            this.Load += new System.EventHandler(this.lab_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.VScrollBar barAddSoluto;
-        private System.Windows.Forms.Button btnSolucao;
+        private System.Windows.Forms.Button btnAddSolucao;
+        private System.Windows.Forms.Button btnRemoveSolucao;
+        private System.Windows.Forms.Label lblSolucao;
+        private System.Windows.Forms.ComboBox cboxSolucoes;
+        private System.Windows.Forms.Label lblSoluto;
+        private System.Windows.Forms.Button btnRemoveSoluto;
+        private System.Windows.Forms.Button btnAddSoluto;
     }
 }
--- Solucoes_fq/lab.cs
+++ Solucoes_fq/lab.cs
@@ -12,24 +12,92 @@ namespace Solucoes_fq
 {
     public partial class lab : Form
     {
+        int solucao, soluto;
+        float massaMolar = 0;
         public lab()
         {
             InitializeComponent();
+
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(solucao < 2000)
+            {
+                solucao += 200;
+                lblSolucao.Text = "Quantidade de solução: " + solucao + "ml";
+                CalcularConcentracao(massaMolar, soluto, solucao);
+            }
         }
 
-        private void barAddSoluto_Scroll(object sender, ScrollEventArgs e)
+        private void btnRemove_Click(object sender, EventArgs e)
         {
-            if(e.OldValue > e.NewValue)
+            if(solucao > 200)
             {
-                btnSolucao.Height--;
-            }
-            else
-            {
-                if(btnSolucao.Height > 10)
+                solucao -= 200;
+                lblSolucao.Text = "Quantidade de solução: " + solucao + "ml";
+                if(CalcularConcentracao(massaMolar, soluto, solucao))
                 {
-                    btnSolucao.Height++;
+
                 }
             }
+        }
+
+        private void btnAddSoluto_Click(object sender, EventArgs e)
+        {
+            if(soluto < 1000)
+            {
+                soluto += 100;
+                lblSoluto.Text = "Quantidade de soluto: " + soluto + " moles";
+            }
+        }
+
+        private void cboxSolucoes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            soluto = 0;
+            switch (cboxSolucoes.Text)
+            {
+                case "Cloreto de cobalto":
+                    BackColor = Color.PaleVioletRed;
+                    break;
+                case "Nitrato de cobalto":
+                    BackColor = Color.Red;
+                    break;
+                case "Cloreto de níquel":
+                    BackColor = Color.Green;
+                    break;
+                case "Sulfato de cobre":
+                    BackColor = Color.Blue;
+                    break;
+            }
+        }
+
+        private void btnRemoveSoluto_Click(object sender, EventArgs e)
+        {
+            if(soluto > 0)
+            {
+                soluto -= 100;
+                lblSoluto.Text = "Quantidade de soluto: " + soluto + " moles";
+            }
+        }
+
+        private void lab_Load(object sender, EventArgs e)
+        {
+            solucao = 200;
+            soluto = 0;
+            lblSolucao.Text = "Quantidade de solução: " + solucao + "ml";
+            lblSoluto.Text = "Quantidade de soluto: " + soluto + " moles";
+            String[] solu = { "Nitrato de cobalto", "Cloreto de cobalto", "Cloreto de níquel", "Sulfato de cobre"};
+            for(int i = 0; i < solu.Length; i++)
+            {
+                cboxSolucoes.Items.Add(solu[i]);
+            }
+        }
+
+        public float CalcularConcentracao(float massaMolar, int quantSoluto, int quantSolucao)
+        {
+            float concentracao = 0;
+            concentracao = quantSoluto / (massaMolar * quantSolucao);
+            return concentracao;
         }
     }
 }
